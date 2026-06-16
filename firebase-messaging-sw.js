0de12ea6038dddfdf8c889746df2f4f7f48b3917
Leuziitos <leeonardobraulino@hotmail.com>
2026-06-16 00:24:01 -0300

    revert: restaura firebase-messaging-sw.js (OneSignal SW)

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,4 @@
+// OneSignal Service Worker
+// Este arquivo mantém o nome firebase-messaging-sw.js para compatibilidade
+// com o SW principal (sw.js), mas agora delega para o OneSignal.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
